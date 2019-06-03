6406fef0f0d4c6096e784978d4a3ed8a4fc19da5
Rodney Wells <rodneywells01@gmail.com>
2019-06-03 19:31:48 -0400

    Fixed Test Execution

--- src/watchlist-view/WatchlistView.test.js
+++ src/watchlist-view/WatchlistView.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import WatchlistView from './WatchlistView';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<WatchlistView />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
