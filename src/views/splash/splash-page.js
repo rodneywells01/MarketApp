3cc3c4583b6827e3803aecc2b3b89ab05338b1d9
Rodney Wells <rodneywells01@gmail.com>
2019-08-24 20:56:22 -0400

    Splash Page Visual Improvements

--- src/views/splash/splash-page.js
+++ src/views/splash/splash-page.js
@@ -5,7 +5,8 @@ import '../../common-styles.css'
 import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
 import ReactDOM from 'react-dom';
-
+import TextField from "@material-ui/core/TextField";
+import Particles from 'react-particles-js';
 import Login from '../../components/Login';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -15,11 +16,16 @@ import WatchlistView from '../watchlist/WatchlistView';
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
-    width: 250
+    width: "100%"
   },
   input: {
-    display: 'none',
+    marginBottom: 10,
+    color: "white"
   },
+  left_view: {
+    width: '50%',
+    border: "1px solid black"
+  }
 }));
 
 function SplashPageView() {
@@ -32,8 +38,15 @@ function SplashPageView() {
   }
 
   const openLogin = () => {
-    setOpen(true)
+    // var email =
   }
+
+  // const _handleTextFieldChange: function(e) {
+  //   this.setState({
+  //       textFieldValue: e.target.value
+  //   });
+  // }
+
 
   const handleClose = () => {
     setOpen(false)
@@ -41,30 +54,40 @@ function SplashPageView() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Marketly</h1>
-        <h4>Curate and Analyze Market Trends</h4>
-
-        <div>
-          <Button variant="contained" color="primary" className={classes.button} onClick={openLogin}>
-            Log In
-          </Button>
-          <br/>
-          <Button variant="contained" color="primary" className={classes.button}>
-            Sign Up for the Beta
-          </Button>
-
+      <div className="flexContainer">
+        <div className="left_view">
+          <div></div>
+          <header className="App-header">
+            <Particles className="background" height="900px" params={{
+              "particles": {
+                  "number": {
+                    "value": 150
+                  }
+              }
+            }}></Particles>
+            <div className="center-me">
+              <h1>Marketly</h1>
+              <h4>Curate and Analyze Market Trends</h4>
+            </div>
+          </header>
         </div>
-
-        <Modal
-          open={open}
-          onClose={handleClose}
-        >
-            <Login></Login>
-        </Modal>
-
-
-      </header>
+        <div className="right_view">
+          <div>
+            <h3>Get Trading</h3>
+            <div style={{width: "70%", display: "inline-block"}}>
+              <TextField className={classes.input} label="Email"></TextField>
+              <TextField className={classes.input} label="Password" type="Password"></TextField>
+              <Button variant="contained" color="primary" className={classes.button} onClick={openLogin}>
+                Log In
+              </Button>
+              <br/>
+              <Button variant="contained" color="primary" className={classes.button}>
+                Sign Up for the Beta
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
